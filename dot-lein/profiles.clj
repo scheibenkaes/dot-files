54bf8e02e94ad75a1cb4df9879fcb50dd6f5abd5
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-01-08 21:06:21 +0100

    update profiles.clj

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -1,2 +1,9 @@
-{:user
- {:plugins [[lein-swank "1.4.4"]]}}
+{:user {:plugins [[lein-midje "3.1.1"]
+                  [lein-ancient "0.5.4"]
+                  [lein-marginalia "0.7.1"]]
+        :dependencies [[clj-stacktrace "0.2.7"]]
+        :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
+                                            'print-cause-trace)
+                           new (ns-resolve (doto 'clj-stacktrace.repl require)
+                                           'pst)]
+                       (alter-var-root orig (constantly (deref new))))]}}
