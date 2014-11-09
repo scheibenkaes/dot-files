f409da8c6fe813fe920e2a8ac137c694f8c4701d
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-11-09 15:56:36 +0100

    add austin to lein profile

--- dot-lein/profiles.clj
+++ dot-lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-midje "3.1.1"]
                   [lein-ancient "0.5.4"]
-                  [lein-marginalia "0.7.1"]]
+                  [lein-marginalia "0.7.1"]
+                  [com.cemerick/austin "0.1.5"]]
         :dependencies [[clj-stacktrace "0.2.7"]]
         :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
                                             'print-cause-trace)
